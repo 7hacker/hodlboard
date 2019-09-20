69ce95281ff42a550d85eb5be26b968610e1acce
Nirmal Thacker <nirmalthacker@gmail.com>
2019-09-20 13:51:24 -0700

    refactored and local testing

--- app/application/routes.py
+++ app/application/routes.py
@@ -2,6 +2,7 @@ import json
 
 from flask import Flask
 from flask_cors import cross_origin
+from flask import current_app as app
 
 
 @app.route('/content')
@@ -22,3 +23,12 @@ def home():
 		mimetype='application/json'
 	)
 	return response
+
+@app.route('/')
+def hello():
+    response = app.response_class(
+        response = json.dumps("hello world"),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return response
