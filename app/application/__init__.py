a4dcc0f7609ff5a3f8a3573990ea91be541c1d4b
Nirmal Thacker <nirmalthacker@gmail.com>
2019-09-20 15:35:11 -0700

    envs

--- app/application/__init__.py
+++ app/application/__init__.py
@@ -3,7 +3,16 @@ from flask import Flask, g
 def create_app():
     """Initialize the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+
+    if app.config["ENV"] == "production":
+        app.config.from_object("config.Production")
+    elif app.config["ENV"] == "staging":
+        app.config.from_object("config.Staging")
+    elif app.config["ENV"] == "development":
+        app.config.from_object("config.Local")
+    else:
+        app.config.from_object("config.Config")
+    #app.config.from_object('config.Config')
 
     with app.app_context():
         # Include our Routes
--- app/config.py
+++ app/config.py
@@ -5,7 +5,9 @@ class Config:
     """
     Base Configuration Vars
     """
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DEBUG = False
+    TESTING = True
+    DATABASE_URI = os.environ.get('LOCAL_DATABASE_URI')
 
 class Production(Config):
     DEBUG = False
@@ -13,6 +15,11 @@ class Production(Config):
     DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URI')
 
 class Staging(Config):
-    DEBUG = True
+    DEBUG = False
     TESTING = True
     DATABASE_URI = os.environ.get('STAGING_DATABASE_URI')
+
+class Local(Config):
+    DEBUG = True
+    TESTING = True
+    DATABASE_URI = os.environ.get('LOCAL_DATABASE_URI')
